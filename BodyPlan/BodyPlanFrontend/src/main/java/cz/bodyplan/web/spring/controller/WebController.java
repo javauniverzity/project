d521bbd99dda2b0c803fd28133813558a5d73ee6
Pavel Lukes <pavel.lukes@etnetera.cz>
2015-04-22 19:10:08 +0200

    mvc ukazka

--- BodyPlan/BodyPlanFrontend/src/main/java/cz/bodyplan/web/spring/controller/WebController.java
+++ BodyPlan/BodyPlanFrontend/src/main/java/cz/bodyplan/web/spring/controller/WebController.java
@@ -8,14 +8,17 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import cz.bodyplan.web.interfaces.component.PasswordComponent;
 import cz.bodyplan.web.interfaces.service.UserService;
 import cz.bodyplan.web.vo.dto.User;
 
 @Controller
+@SessionAttributes("user")
 public class WebController {
 
 	@Autowired
@@ -25,21 +28,30 @@ public class WebController {
 	PasswordComponent passwordComponent;
 
 	@RequestMapping(value = "index.html", method = RequestMethod.GET)
-	public String index(final HttpServletRequest req, final HttpServletResponse res, final Model model) {
+	public String index(final HttpServletRequest req,
+			final HttpServletResponse res, final Model model) {
 		final User user = new User();
-		
+
 		user.setUsername("jan-" + new Date().getTime());
 		user.setFirstName("Pinger");
-	
+
 		Hlavni.main(null);
 		user.setLastName(TestIP.nedostupne);
-		
+
 		userService.create(user);
-		return "index";		
+		model.addAttribute("user", user);
+		return "index";
+	}
+
+	@RequestMapping(value = "editUser.html", method = RequestMethod.POST)
+	public String editUser(@ModelAttribute("user") final User user) {
+		userService.update(user);
+		return "index";
 	}
 
 	@RequestMapping(value = "init.html", method = RequestMethod.GET)
-	public String init(final HttpServletRequest req, final HttpServletResponse res, final Model model) {
+	public String init(final HttpServletRequest req,
+			final HttpServletResponse res, final Model model) {
 		final User admin = new User();
 		admin.setUsername("admin");
 		admin.setPassword(passwordComponent.getMd5Password(admin, "admin"));
@@ -48,7 +60,8 @@ public class WebController {
 	}
 
 	@RequestMapping(value = "error.html", method = RequestMethod.GET)
-	public String error(final HttpServletRequest req, final HttpServletResponse res, final Model model) {
+	public String error(final HttpServletRequest req,
+			final HttpServletResponse res, final Model model) {
 		return "index";
 	}
 
--- BodyPlan/BodyPlanFrontend/src/main/webapp/WEB-INF/pages/index.jsp
+++ BodyPlan/BodyPlanFrontend/src/main/webapp/WEB-INF/pages/index.jsp
@@ -1,1 +1,20 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ page import="java.util.Calendar"%>
+<%@ page import="org.springframework.context.i18n.LocaleContextHolder"%>
+<%@ page import="java.text.SimpleDateFormat"%>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="spring"%>
+<%@ taglib prefix="security" uri="http://www.springframework.org/security/tags" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+
 test2
+${user.username}
+
+<form:form commandName="user" action="editUser.html">
+	<form:input path="firstName"/>
+	
+	<form:button>submit</form:button>
+
+</form:form>
