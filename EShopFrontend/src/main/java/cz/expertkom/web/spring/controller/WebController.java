4c0805d6ae169d4b5c913c4048c6b5cf8ec1dc07
Honzik <Honzik@10.0.0.32>
2015-05-05 14:46:53 +0200

    Commituji controller, je to jenom pár řádků, až z toho mám blbej pocit, jestli jsem na něco nezapomněl. Tak mě kdyžtak upozorněte :)

--- EShopFrontend/src/main/java/cz/expertkom/web/spring/controller/WebController.java
+++ EShopFrontend/src/main/java/cz/expertkom/web/spring/controller/WebController.java
@@ -1,16 +1,68 @@
 package cz.expertkom.web.spring.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+import cz.expertkom.web.interfaces.service.DatabaseService;
+import cz.expertkom.web.vo.dto.Database;
+
+@SessionAttributes("database")
 @Controller
 public class WebController {
 
+	@Autowired
+	DatabaseService databaseService;
+
 	@RequestMapping(value = "index", method = RequestMethod.GET)
-	public String index() {
+	public String index(final Model model) {
+		List<Database> products = databaseService.getList();
+		model.addAttribute("products", products);
 		return "index";
 	}
+		
+	/* TODO Prozatím k nièemu, pøípravná konstrukce pro admin stránku ke správì databáze pøes web
+
+	@RequestMapping(value = "adminDatabase", method = RequestMethod.GET)
+	public String adminDatabase(final Model model) {
+		List<Database> products = databaseService.getList();
+		model.addAttribute("products", products);
+		return "adminDatabase";
+	}
+
+	@RequestMapping(value = "newProduct", method = RequestMethod.GET)
+	public String newProduct(final Model model) {
+		Database database = new Database();
+		model.addAttribute("database", database);
+		return "adminDatabase";
+	}
+
+	@RequestMapping(value = "makeProduct", method = RequestMethod.POST)
+	public String makeProduct(@ModelAttribute("database") final Database database) {
+		databaseService.create(database);
+		return "adminDatabase";
+	}
+
+	@RequestMapping(value = "editProduct", method = RequestMethod.GET)
+	public String editProduct(final Model model, @RequestParam(required = true, value = "Id") Long Id) {
+		Database database = null;
+		database = databaseService.loadById(Id);
+		model.addAttribute("database", database);
+		return "editProduct";
+	}
+
+	@RequestMapping(value = "editingProduct", method = RequestMethod.POST)
+	public String editingProduct(@ModelAttribute("product") final Database database) {
+
+		databaseService.update(database);
+		return "editProduct";
+	}
+	*/
 
 	@RequestMapping(value = "error", method = RequestMethod.GET)
 	public String error() {
