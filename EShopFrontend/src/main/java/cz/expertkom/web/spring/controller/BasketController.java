9d9a7e590bc6b93ccc0d03228b5c4a5f77f1415c
Honzik <honzatt@seznam.cz>
2015-05-30 15:13:38 +0200

    Controller comments part 2

--- EShopFrontend/src/main/java/cz/expertkom/web/spring/controller/BasketController.java
+++ EShopFrontend/src/main/java/cz/expertkom/web/spring/controller/BasketController.java
@@ -17,10 +17,13 @@ import cz.expertkom.web.interfaces.service.UserService;
 import cz.expertkom.web.vo.dto.Product;
 import cz.expertkom.web.vo.dto.User;
 
+/**
+ * Controller øeší funkcionalitu nákupního košíku
+ */
+
 @SessionAttributes({ "user", "basket" })
 @Controller
 public class BasketController {
-
 
 	@Autowired
 	UserService userService;
@@ -28,16 +31,21 @@ public class BasketController {
 	@Autowired
 	ProductService productService;
 
+	// Vytvoøí košík, pøiøadí ho existujícímu userovi a na zálkladì ID mu
+	// pøiøadí produkt. Vše pošle do session
 	@RequestMapping(value = "addToBasket", method = RequestMethod.GET)
 	public String addToBasket(final Model model,
 			@RequestParam("id") String productID,
 			@ModelAttribute("user") User user) {
 
+		// Pokud košík neexistuje, vytvoøíme ho
 		List<Product> basket = user.getBasket();
 		if (basket == null) {
 			basket = new ArrayList<Product>();
 			user.setBasket(basket);
 		}
+
+		// Naèteme produkt dle ID, vložíme ho do košíku a pošleme do session
 		Product product = productService.loadById(Long.valueOf(productID));
 		basket.add(product);
 		model.addAttribute("basket", basket);
@@ -46,25 +54,31 @@ public class BasketController {
 		return "basket";
 	}
 
-//	@RequestMapping(value = "showBasket", method = RequestMethod.GET)
-//	public String showBasket(final Model model,
-//			@ModelAttribute("User") User user) {
-//
-//		List<Database> basket = databaseService.getList();
-//		model.addAttribute("basket", basket);
-//
-//		return "basket";
-//	}
+	// Nepoužívá se
+	// @RequestMapping(value = "showBasket", method = RequestMethod.GET)
+	// public String showBasket(final Model model,
+	// @ModelAttribute("User") User user) {
+	//
+	// List<Database> basket = databaseService.getList();
+	// model.addAttribute("basket", basket);
+	//
+	// return "basket";
+	// }
 
+	// Závazná objednávka. Pokud k ní dojde, snížíme skladové zásoby
 	@RequestMapping(value = "order", method = RequestMethod.GET)
 	public String order(@ModelAttribute("user") User user) {
 
+		// Vytvoøíme prázdný list IDèek a list produktù v košíku
 		List<Long> ids = new ArrayList<Long>();
 		List<Product> basket = user.getBasket();
 
+		// do listu IDèek pøiøadíme ID produktù v košíku
 		for (Product product : basket) {
 			ids.add(product.getId());
 		}
+
+		// Snížíme skladové zásoby u produktù dle listu IDèek
 		productService.decreaseQuantity(ids);
 
 		return "index";
--- EShopFrontend/src/main/java/cz/expertkom/web/spring/controller/LoginController.java
+++ EShopFrontend/src/main/java/cz/expertkom/web/spring/controller/LoginController.java
@@ -10,29 +10,34 @@ import cz.expertkom.web.interfaces.component.PasswordComponent;
 import cz.expertkom.web.interfaces.service.AuthorityService;
 import cz.expertkom.web.interfaces.service.UserService;
 
+/**
+ * Controller pro stránku s pøihlášením
+ */
+
 @Controller
 public class LoginController {
-	
+
 	@Autowired
 	AuthorityService authorityService;
 
 	@Autowired
 	PasswordComponent passwordComponent;
-	
+
 	@Autowired
 	UserService userService;
-	
+
 	@RequestMapping(value = "login", method = RequestMethod.GET)
 	public String index(final Model model) {
-//		User user = new User();
-//		user.setUsername("admin");
-//		user.setPassword(passwordComponent.getMd5Password(user, "admin"));
-//		Set<Authority> authorities = new HashSet<Authority>();
-//		authorities.add(authorityService.getAuthority("ROLE_ADMIN"));
-//		user.setAuthorities(authorities);
-//		userService.create(user);
+		
+		// Zakomentováno, slouží k vytvoøení Admina jednoducým naètením stránky
+		// User user = new User();
+		// user.setUsername("admin");
+		// user.setPassword(passwordComponent.getMd5Password(user, "admin"));
+		// Set<Authority> authorities = new HashSet<Authority>();
+		// authorities.add(authorityService.getAuthority("ROLE_ADMIN"));
+		// user.setAuthorities(authorities);
+		// userService.create(user);
+		
 		return "login";
 	}
-	
-
 }
